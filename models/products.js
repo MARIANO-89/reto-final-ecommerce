467276e5c5f23bc7cf431c59d5dcff67763cc66a
MARIANO-89 <nano_carp@hotmail.com>
2021-10-02 16:28:52 +0200

    Create products model

---
+++ models/products.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../bd_connection');
+const Users = require('./users');
+
+const Products = sequelize.define('products', {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    owner: {
+        type: DataTypes.INTEGER,
+        references: { model: Users, key: 'id' },
+    },
+    created: {
+        type: DataTypes.DATE,
+    },
+    timestamps: false,
+});
+
+module.exports = Products;
